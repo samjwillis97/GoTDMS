a824ea75f438417f194ff0ed6a962c37d0287244
Sam Willis <sam@debian-xps-9550.debian-xps-9550.local>
2021-09-30 08:26:25 +1000

    in the middle of re-arranging code

--- pkg/tdms/reader.go
+++ pkg/tdms/reader.go
@@ -1,6 +1,13 @@
 package tdms
 
 import (
+	"encoding/binary"
+	"io"
+	"os"
+	"fmt"
+	"strings"
+	"bytes"
+
 	log "github.com/sirupsen/logrus"
 )
 
@@ -30,6 +37,31 @@ const (
 	DAQmx      tdsDataType = 0xFFFFFF
 )
 
+const (
+	kTocMetaData        uint32 = 0x2
+	kTocRawData         uint32 = 0x8
+	kTocDAQmxRawData    uint32 = 0x80
+	kTocInterleavedData uint32 = 0x20
+	kTocBigEndian       uint32 = 0x40
+	kTocNewObjList      uint32 = 0x4
+)
+
+type LeadInData struct {
+	ToCMask       uint32
+	versionNumber uint32
+	nextSegOffset uint64
+	rawDataOffset uint64
+	nextSegPos    uint64
+	dataPos       uint64
+}
+
+var (
+	noRawDataValue            = []byte{255, 255, 255, 255}
+	matchesPreviousValue      = []byte{0, 0, 0, 0}
+	daqmxFormatChangingScaler = []byte{69, 12, 00, 00}
+	daqmxDigitalLineScaler    = []byte{69, 13, 00, 00}
+)
+
 type RawDataIndex struct {
 	dataType       tdsDataType
 	arrayDimension uint32
@@ -42,38 +74,446 @@ type SegmentObject struct {
 	rawDataIndex       RawDataIndex
 }
 
-// REQUIRES
-// ObjMap/Segment.objects
-// segment.nextSegPos
-// segment.dataPos
-func calculateChunks(objects map[string]SegmentObject, nextSegPos uint64, dataPos uint64) uint64 {
-	dataSize := uint64(0)
-	for _, e := range objects {
-		dataSize += e.rawDataIndex.rawDataSize
+type Property struct {
+	name          string
+	dataType      tdsDataType
+	valuePosition int64
+	stringValue   string
+}
+
+// Sorting for Properties
+func (p Properties) Len() int { return len(p) }
+
+func (p Properties) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
+
+func (p Properties) Less(i, j int) bool {
+	var si string = p[i].name
+	var sj string = p[j].name
+	var si_low = strings.ToLower(si)
+	var sj_low = strings.ToLower(sj)
+	if si_low == sj_low {
+		return si < sj
 	}
-	log.Debugf("Data Size: %d", dataSize)
+	return si_low < sj_low
+}
 
-	totalDataSize := nextSegPos - dataPos
-	log.Debugf("Total Data Size: %d", totalDataSize)
+type Properties []Property
 
-	if dataSize < 0 || totalDataSize < 0 {
-		log.Fatal("Negative data size")
-	} else if dataSize == 0 {
-		// npTDMS: sometimes kTocRawData is set, but there isn't actually any data
-		if totalDataSize != dataSize {
-			log.Fatal("Zero channel data size but data length")
+type Segment struct {
+	position                 uint64
+	numChunks                uint64
+	objects                  map[string]SegmentObject
+	objectOrder              []string
+	kToCMask                 uint32
+	nextSegPos               uint64
+	dataPos                  uint64
+	finalChunkLengthOverride uint64
+	objectIndex              uint64
+	propMap                  map[string]map[string]Property
+}
+
+// Reads the TDMS Lead-In (28 Bytes) of a Segment
+// Includes:
+// - Start Tag = 4 Bytes
+// - ToC BitMask = 4 Bytes
+// - Version Number = 4 Bytes
+// - Segment Length = 8 Bytes
+// - Metadata Length = 8 Bytes
+// Total 28 Bytes
+// Starts at Byte Defined by Offset
+// When is the reference point for the offset
+// 0 = Beginning of File
+// 1 = Current Position
+// 2 = End of File
+//
+// Returns LeadInData
+func readTDMSLeadIn(file *os.File, offset int64, whence int) LeadInData {
+	segmentStartPos, err := file.Seek(offset, whence)
+	if err != nil {
+		log.Fatal("Error return from file.Seek in readTDMSLeadIn: ", err)
+	}
+
+	log.Debugln("READING LEAD-IN")
+
+	// Starts with a 4-byte tag that identifies a TDMS Segment ("TDSm")
+	segStartTag := make([]byte, 4)
+	_, err = io.ReadFull(file, segStartTag)
+	if err != nil {
+		log.Fatal("Error return from io.ReadFull in readTDMSLeadIn: ", err)
+	}
+	if string(segStartTag) != "TDSm" {
+		log.Fatal("Segment is not a TDMS")
+	}
+	log.Debugln("Valid TDMS Segment Starting at: ", segmentStartPos)
+
+	// 4 Byte ToC BitMask
+	// Example
+	// Binary (Hexadecimal)		= 0E 00 00 00
+	// ToC Mask								= 0x0000000E = 0b1110 = 0001 0001 0001 0000
+	// Segment Contains: Object List, Meta Data, Raw Data
+	tocBitMaskBytes := make([]byte, 4)
+	_, err = io.ReadFull(file, tocBitMaskBytes)
+	if err != nil {
+		log.Fatal("Error return from io.ReadFull in readTDMSLeadIn: ", err)
+	}
+	tocBitMask := binary.LittleEndian.Uint32(tocBitMaskBytes)
+	log.Debugln("ToC BitMask: ", tocBitMask)
+	if (kTocMetaData & tocBitMask) == kTocMetaData {
+		log.Debugln("Segment Contains Meta Data")
+	}
+	if (0b1000 & tocBitMask) == 0b1000 {
+		log.Debugln("Segment Contains Raw Data")
+	}
+	if (0b10000000 & tocBitMask) == 0b10000000 {
+		log.Debugln("Segment Contains DAQmx Raw Data")
+	}
+	if (0b100000 & tocBitMask) == 0b100000 {
+		log.Debugln("Segment Contains Interleaved Data")
+	}
+	if (0b1000000 & tocBitMask) == 0b1000000 {
+		log.Debugln("Segment Contains Big Endian Data")
+	}
+	if (0b100 & tocBitMask) == 0b100 {
+		log.Debugln("Segment Contains New Object List")
+	}
+
+	// 4 Byte Version Number
+	// 4713 = v2.0
+	// 4712 = Older
+	versionNumber := Uint32FromTDMS(file, 0, 1)
+	log.Debugln("Version Number: ", versionNumber)
+
+	// 8 Bytes - Length of Remaining Segment
+	// Also known as Next Segment Offset
+	// Remaining Length = Overall Length of Segment - Length of Lead in ()
+	// If an application encounters a problem writing, all bytes will = 0xFF
+	// can only happen at EOF
+	segLength := Uint64FromTDMS(file, 0, 1)
+	log.Debugln("Segment Length: ", segLength)
+
+	// 8 Bytes - Length of Metadata in Segment
+	// Also known as raw data offset
+	// If segment contains no metadata will = 0
+	metaLength := Uint64FromTDMS(file, 0, 1)
+	log.Debugln("Metadata Length: ", metaLength)
+
+	leadInSize := uint64(28)
+
+	nextSegPos := uint64(0)
+	if segLength == 0xFFFFFFFFFFFFFFFF {
+		log.Debugf("Segment incomplete, attempting to Read")
+		fileStat, err := file.Stat()
+		if err != nil {
+			log.Fatal("Error return by file.Stat() in readTDMSLeadIn: ", err)
 		}
-		numChunks := uint64(0)
-		return numChunks
+		nextSegPos = uint64(fileStat.Size())
+	} else {
+		nextSegPos = uint64(segmentStartPos) + segLength + leadInSize
 	}
 
-	// Checking for Multiple
-	chunkRemainder := totalDataSize % dataSize
-	if chunkRemainder == 0 {
-		numChunks := uint64(totalDataSize / dataSize)
-		return numChunks
+	dataPos := uint64(segmentStartPos) + leadInSize + metaLength
+
+	return LeadInData{
+		tocBitMask,
+		versionNumber,
+		segLength,
+		metaLength,
+		nextSegPos,
+		dataPos,
+	}
+}
+
+// Read the TDMS MetaData of a Segment
+//
+// Includes:
+// - Reading Number of Objects in Segment
+// - Object Paths
+// - Object Info
+// - Object Properties
+//
+// Starts at Byte Defined by Offset
+// Whence is the reference point for the offset
+// 0 = Beginning of File
+// 1 = Current Position
+// 2 = End of File
+//
+// Returns Segment Objects and Properties
+func readTDMSMetaData(file *os.File, offset int64, whence int, leadin LeadInData, prevSegment Segment, allPrevSegObjs map[string]SegmentObject) (map[string]SegmentObject, []string, map[string]map[string]Property) {
+	_, err := file.Seek(offset, whence)
+	if err != nil {
+		log.Fatal("Error return from file.Seek in readTDMSObject: ", err)
+	}
+
+	// Initialize Empty Map for Objects
+	objMap := make(map[string]SegmentObject)
+	objOrder := make([]string, 0)
+
+	// Init Map of Property Maps
+	propertyMap := make(map[string]map[string]Property)
+
+	// True if no MetaData
+	if (kTocMetaData & leadin.ToCMask) != kTocMetaData {
+		log.Debugln("Reuse Previous Segment Metadata")
+		return prevSegment.objects, prevSegment.objectOrder, prevSegment.propMap
+	}
+
+	// TODO: Big Endianness with TocMask
+
+	prevSegObjectNum := len(prevSegment.objects)
+
+	if ((kTocNewObjList & leadin.ToCMask) == kTocNewObjList) || prevSegObjectNum == 0 {
 	} else {
-		log.Fatal("Data Size is not a multiple of Chunk Size")
-		return uint64(0)
+		// There can be a list of new objects that are appended,
+		// or previous objects that are repeated with changed properties
+		objMap = prevSegment.objects
+		objOrder = prevSegment.objectOrder
+	}
+
+	log.Debugln("READING METADATA")
+
+	// First 4 Bytes have number of objects in metadata
+	numObjects := Uint32FromTDMS(file, 0, 1)
+	log.Debugln("Number of Objects: ", numObjects)
+
+	// ar objects = make([]string, numObjects)
+	for i := uint32(0); i < numObjects; i++ {
+		log.Debugf("Reading Object %d \n", i)
+
+		// Read Object Path
+		objPath := StringFromTDMS(file, 0, 1)
+		log.Debugf("Object %d Path: %s\n", i, objPath)
+
+		// Read Raw Data Index/Length of Index Information
+		// FF FF FF FF means there is no raw data
+		// 69 12 00 00 DAQmx Format Changing Scaler
+		// 69 13 00 00 DAQmx Digital Line Scaler
+		// Matches Previous Segment Same Object i.e. use previous
+		// Otherwise
+		rawDataIndexHeaderBytes := make([]byte, 4)
+		_, err := io.ReadFull(file, rawDataIndexHeaderBytes)
+		if err != nil {
+			log.Fatal("Error return from io.ReadFull in readTDMSObject: ", err)
+		}
+		log.Debugf("Object Raw Data Index: % x", rawDataIndexHeaderBytes)
+
+		// check if we already have objPath in Map
+		// only executes if present true, present will be true if found in map
+		// first value is the value found
+		if val, present := objMap[objPath]; present {
+			log.Debugf("Updating Existing %s Object\n", objPath)
+			// Current Header says No Data
+			if bytes.Compare(rawDataIndexHeaderBytes, noRawDataValue) == 0 {
+				// Matched Header says Has Data
+				if bytes.Compare(val.rawDataIndexHeader, noRawDataValue) != 0 {
+					objMap[objPath] = SegmentObject{
+						rawDataIndexHeaderBytes,
+						val.rawDataIndex,
+					}
+				}
+				// Current Header Matches Previous
+			} else if bytes.Compare(rawDataIndexHeaderBytes, matchesPreviousValue) == 0 {
+				// Previous has No Raw Data
+				if bytes.Compare(val.rawDataIndexHeader, noRawDataValue) == 0 {
+					objMap[objPath] = SegmentObject{
+						rawDataIndexHeaderBytes,
+						val.rawDataIndex,
+					}
+				}
+				// New Segment Metadata OR Updates to Existing Data
+			} else {
+				objMap[objPath] = SegmentObject{
+					rawDataIndexHeaderBytes,
+					readTDMSRawDataIndex(file, 0, 1, rawDataIndexHeaderBytes),
+				}
+			}
+		} else if val, present := allPrevSegObjs[objPath]; present {
+			log.Debugf("Reusing Previous %s Object\n", objPath)
+			// reuse previous object
+			if bytes.Compare(rawDataIndexHeaderBytes, noRawDataValue) == 0 {
+				// Reuse Segment  But Leave Data Index Information as Set Previously
+				if bytes.Compare(val.rawDataIndexHeader, noRawDataValue) == 0 {
+					// Previous Segment has Data
+					// Copy Previos to Current, Leaving Header
+					objMap[objPath] = SegmentObject{
+						rawDataIndexHeaderBytes,
+						val.rawDataIndex,
+					}
+					objOrder = append(objOrder, objPath)
+				} else {
+					// Copy Completely
+					objMap[objPath] = val
+					objOrder = append(objOrder, objPath)
+				}
+				// Matches Previous
+			} else if bytes.Compare(rawDataIndexHeaderBytes, matchesPreviousValue) == 0 {
+				if bytes.Compare(val.rawDataIndexHeader, noRawDataValue) != 0 {
+					// Copy Previos to Current, Leaving Header
+					objMap[objPath] = SegmentObject{
+						rawDataIndexHeaderBytes,
+						val.rawDataIndex,
+					}
+					objOrder = append(objOrder, objPath)
+				} else {
+					// Copy Completely
+					objMap[objPath] = val
+					objOrder = append(objOrder, objPath)
+				}
+			} else {
+				// Changed Metadata in this Section
+				objMap[objPath] = SegmentObject{
+					rawDataIndexHeaderBytes,
+					readTDMSRawDataIndex(file, 0, 1, rawDataIndexHeaderBytes),
+				}
+				objOrder = append(objOrder, objPath)
+			}
+		} else {
+			log.Debugf("New Segment Object: %s\n", objPath)
+			// New Segment Object
+			if bytes.Compare(rawDataIndexHeaderBytes, matchesPreviousValue) == 0 {
+				log.Fatalln("Raw Data Index says to reuse previous, though this object has not been seen before: ", objPath)
+			} else if bytes.Compare(rawDataIndexHeaderBytes, noRawDataValue) != 0 {
+				objMap[objPath] = SegmentObject{
+					rawDataIndexHeaderBytes,
+					readTDMSRawDataIndex(file, 0, 1, rawDataIndexHeaderBytes),
+				}
+				objOrder = append(objOrder, objPath)
+			} else {
+				objMap[objPath] = SegmentObject{
+					rawDataIndexHeaderBytes,
+					RawDataIndex{
+						0,
+						0,
+						0,
+						0,
+					},
+				}
+				objOrder = append(objOrder, objPath)
+			}
+		}
+
+		// Number of Object Properties
+		numProperties := Uint32FromTDMS(file, 0, 1)
+		log.Debugf("Number of Object %d Properties: %d\n", i, numProperties)
+
+		// Read Properties
+		for j := uint32(0); j < numProperties; j++ {
+			log.Debugf("Reading Object %d Property %d\n", i, j)
+			property := readTDMSProperty(file, 0, 1)
+			// if propMap, present := propertyMap[objPath]; present {
+			if _, present := propertyMap[objPath]; present {
+				// Property Maps Exists for Path
+				propertyMap[objPath][property.name] = property
+			} else {
+				// Property Map Doesn't exist for Path yet
+				initMap := map[string]Property{
+					property.name: {
+						property.name,
+						property.dataType,
+						property.valuePosition,
+						property.stringValue,
+					},
+				}
+				propertyMap[objPath] = initMap
+			}
+		}
+	}
+	return objMap, objOrder, propertyMap
+}
+
+// Reads Raw Data Index of a Segment Object
+//
+// Returns RawDataIndex
+func readTDMSRawDataIndex(file *os.File, offset int64, whence int, rawDataIndexHeader []byte) RawDataIndex {
+	_, err := file.Seek(offset, whence)
+	if err != nil {
+		log.Fatal("Error return by file.Seek in readTDMSRawDataIndex: ", err)
+	}
+
+	indexLength := binary.LittleEndian.Uint32(rawDataIndexHeader)
+	log.Debugf("Object Index Length: %d\n", indexLength)
+
+	dataType := tdsDataType(Uint32FromTDMS(file, 0, 1))
+	log.Debugf("Object Data Type: %d\n", dataType)
+
+	// must equal 1 for v2.0
+	arrayDimension := Uint32FromTDMS(file, 0, 1)
+	if arrayDimension != 1 {
+		log.Fatal("Not Valid TDMS 2.0, Data Dimension is not 1")
+	}
+
+	numValues := Uint64FromTDMS(file, 0, 1)
+	log.Debugf("Object Number of Values: %d\n", numValues)
+
+	dataSize := 0
+	switch tdsDataType(dataType) {
+	case Int8, Uint8, Boolean:
+		dataSize = 1
+	case Int16, Uint16:
+		dataSize = 2
+	case Int32, Uint32, SGL, SGLwUnit:
+		dataSize = 4
+	case Int64, Uint64, DBL, DBLwUnit:
+		dataSize = 8
+	case Timestamp:
+		dataSize = 16
+	}
+
+	channelRawDataSize := uint64(dataSize) * uint64(arrayDimension) * numValues
+	log.Debugf("Channel Raw Data Size: %d\n", channelRawDataSize)
+
+	return RawDataIndex{
+		tdsDataType(dataType),
+		arrayDimension,
+		numValues,
+		channelRawDataSize,
+	}
+}
+
+// Reads a single property from a Segment Object
+func readTDMSProperty(file *os.File, offset int64, whence int) Property {
+	_, err := file.Seek(offset, whence)
+	if err != nil {
+		log.Fatal("Error return from file.Seek in readTDMSObject: ", err)
+	}
+
+	// Property Name
+	propertyName := StringFromTDMS(file, 0, 1)
+	// log.Debugf("Property Name: %s\n", propertyName)
+
+	// Debuged in Hex
+	propertyDataType := Uint32FromTDMS(file, 0, 1)
+	propertyTdsDataType := tdsDataType(propertyDataType)
+
+	// Position for reading later
+	valuePosition, _ := file.Seek(0, 1)
+
+	// Property Value coerced to String
+	var valueString string
+
+	// TODO
+	// Finish This
+	// Converts to Properties
+	switch propertyTdsDataType {
+	default:
+		log.Fatal("Property Data Type Unkown")
+	case String:
+		valueString = StringFromTDMS(file, 0, 1)
+	case Int32:
+		valueString = fmt.Sprintf("%d", Int32FromTDMS(file, 0, 1))
+	case Uint32:
+		valueString = fmt.Sprintf("%d", Uint32FromTDMS(file, 0, 1))
+	case Uint64:
+		valueString = fmt.Sprintf("%d", Uint64FromTDMS(file, 0, 1))
+	case DBL:
+		valueString = fmt.Sprintf("%e", DBLFromTDMS(file, 0, 1))
+	case Timestamp:
+		valueString = TimeFromTDMS(file, 0, 1).String()
+	}
+
+	return Property{
+		propertyName,
+		propertyTdsDataType,
+		valuePosition,
+		valueString,
 	}
 }
--- pkg/tdms/utils.go
+++ pkg/tdms/utils.go
@@ -312,3 +312,39 @@ func TimeFromTDMS(file *os.File, offset int64, whence int) time.Time {
 	}
 	return timeValue
 }
+
+// REQUIRES
+// ObjMap/Segment.objects
+// segment.nextSegPos
+// segment.dataPos
+func calculateChunks(objects map[string]SegmentObject, nextSegPos uint64, dataPos uint64) uint64 {
+	dataSize := uint64(0)
+	for _, e := range objects {
+		dataSize += e.rawDataIndex.rawDataSize
+	}
+	log.Debugf("Data Size: %d", dataSize)
+
+	totalDataSize := nextSegPos - dataPos
+	log.Debugf("Total Data Size: %d", totalDataSize)
+
+	if dataSize < 0 || totalDataSize < 0 {
+		log.Fatal("Negative data size")
+	} else if dataSize == 0 {
+		// npTDMS: sometimes kTocRawData is set, but there isn't actually any data
+		if totalDataSize != dataSize {
+			log.Fatal("Zero channel data size but data length")
+		}
+		numChunks := uint64(0)
+		return numChunks
+	}
+
+	// Checking for Multiple
+	chunkRemainder := totalDataSize % dataSize
+	if chunkRemainder == 0 {
+		numChunks := uint64(totalDataSize / dataSize)
+		return numChunks
+	} else {
+		log.Fatal("Data Size is not a multiple of Chunk Size")
+		return uint64(0)
+	}
+}
